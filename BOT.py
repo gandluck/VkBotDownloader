f490c9831e450ee940ec02ac8a8576ef1992dac0
gandluck <gandluckprivat@gmail.com>
2023-05-10 17:29:07 +0300

    test
    test

---
+++ BOT.py
@@ -0,0 +1,46 @@
+from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN_API, start_text
+from yt_dlp import YoutubeDL
+import os.path
+from moviepy.editor import *
+
+bot = Bot(TOKEN_API)
+dp = Dispatcher(bot)
+COUNTER_DIR = 0
+cwd = os.getcwd() # сохраняем текущую директорию
+
+@dp.message_handler(commands='start')
+async def start(message: types.Message):
+    await message.answer(text=start_text)
+
+
+@dp.message_handler()
+async def send_video(message: types.Message):
+    global COUNTER_DIR
+    os.mkdir(str(COUNTER_DIR)) # Создаем новую директорию
+    new_path = os.path.abspath(str(COUNTER_DIR))  # сохраняем путь новой директории
+    os.chdir(str(COUNTER_DIR)) # Выполняем код в новой директории
+    COUNTER_DIR += 1
+
+    with YoutubeDL() as ydl:
+        os.system(f'yt-dlp {message.text}')
+        # заходим в директорию и берем путь к файлу
+        file_name = os.listdir(new_path)[0]
+        full_path = os.path.join(new_path, file_name)
+
+        # путь к новому файлу
+        new_file = os.path.join('new_video.mp4')
+
+        video = VideoFileClip(full_path)  # достаём аудиодорожку
+        audio = video.audio
+        audio.write_audiofile('audio.wav')
+        recovered_video = video.resize(width=video.w, height=video.h)  # Восстанавливаем поврежденный файл
+        recovered_video.write_videofile('video_to_send.mp4')
+        await bot.send_video(chat_id=message.from_user.id,
+                             video=open('video_to_send.mp4', 'rb'))
+        # возвращаемся в предыдущую директорию
+        os.chdir(cwd)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
